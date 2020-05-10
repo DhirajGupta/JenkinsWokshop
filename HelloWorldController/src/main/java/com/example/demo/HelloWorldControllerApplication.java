2b1d36ae8ca6233dcf107fb8d66e7aed6a20b638
DHIRAJ <TL@gmail.com>
2020-05-10 19:20:21 +0530

    first change for jenkins

--- HelloWorldController/src/main/java/com/example/demo/HelloWorldControllerApplication.java
+++ HelloWorldController/src/main/java/com/example/demo/HelloWorldControllerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController{
+public class HelloWorldControllerApplication{
 @RequestMapping(value="/")
 public String returnHello(){
 return "welcome to jenkins world";
