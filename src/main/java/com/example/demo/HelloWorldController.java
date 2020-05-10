eada4a521987e2deef0b33d263a4d00090bdfd5a
DHIRAJ <TL@gmail.com>
2020-05-10 19:10:21 +0530

    first commit for jenkins

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController{
+@RequestMapping(value="/")
+public String returnHello(){
+return "welcome to jenkins world";
+}
+}
